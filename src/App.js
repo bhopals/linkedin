0fd77b8eea4c4a47b9a025590adc63c3d5063b31
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-29 19:27:57 -0400

    add `Router` and `Login` Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Login from './components/Login'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h2> Let us build linkedIn Clone</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
