9cab24f3daec255908ba21c807fb56dac35dd715
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-30 13:32:04 -0400

    add Navbar on Homepage with all NavList options

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,175 @@
 import styled from 'styled-components'
 
 const Header = (props) => {
-    return <div>HEADER</div>
+    return (
+        <Container>
+            <Content>
+                <Logo><a href="/home">
+                    <img src="/images/home-logo.svg" /></a>
+                </Logo>
+                <Search>
+                    <div>
+                        <input type="text" placeholder="Search"></input>
+                    </div>
+                    <SearchIcon>
+                        <img src="/images/search-icon.svg"  alt="" />
+                    </SearchIcon>
+                </Search>
+                <Nav>
+                    <NavListWarp>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-home.svg" alt="" />
+                                <span>Home</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-network.svg" alt="" />
+                                <span>Network</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-jobs.svg" alt="" />
+                                <span>Jobs</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-messaging.svg" alt="" />
+                                <span>Messaging</span>
+                            </a>
+                        </NavList>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-notifications.svg" alt="" />
+                                <span>Notifications</span>
+                            </a>
+                        </NavList>
+                    </NavListWarp>
+                </Nav>
+            </Content>
+        </Container>
+    )
 }
+
+const Container = styled.div`
+    background-color: white;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+    left: 0;
+    padding: 0 24px;
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    z-index: 100;
+`;
+
+const Content = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    min-height: 100%;
+    max-width: 1128px;
+`;
+
+const Logo = styled.span`
+    font-size: 0px;
+    margin-right: 8px;
+
+`;
+
+const SearchIcon = styled.div`
+    width: 40px;
+    position: absolute;
+    z-index: 1;
+    top: 10px;
+    left: 2px;
+    border-radius: 0 2px 2px 0;
+    margin: 0;
+    pointer-events: none;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+`;
+
+const Search = styled.div`
+    opacity: 1;
+    flex-grow: 1;
+    position: relative;
+    & > div {
+        max-width: 280px;
+        input {
+            border: none;
+            box-shadow: none;
+            background-color: #eef3f8;
+            border-radius: 2px;
+            color: rgba(0, 0, 0, 0.9);
+            width: 218px;
+            padding: 0 8px 0 40px;
+            line-height: 1.75;
+            font-weight: 400;
+            font-size:  14px;
+            height: 34px;
+            border-color: #dec6f1;
+            vertical-align: text-top;
+        }
+    }
+`;
+
+const Nav = styled.nav`
+    margin-left: auto;
+    display: block;
+    @media(max-width: 768px) {
+        position: fixed;
+        left:0;
+        bottom: 0;
+        background: white;
+        width: 100%;
+    }
+`;
+
+const NavListWarp = styled.ul`
+    display: flex;
+    flex-wrap: nowrap;
+    list-style-type: none;
+`;
+
+const NavList = styled.li`
+    display: flex;
+    align-items: center;
+    a {
+        align-items: center;
+        background: transparent;
+        display: flex;
+        flex-direction:  column;
+        font-size: 12px;
+        font-weight: 400;
+        justify-content: center;
+        line-height: 1.5;
+        min-height: 42px;
+        min-width:  80px;
+        text-decoration: none;
+
+        span {
+            color: rgba(0, 0, 0, 0.6);
+            display: flex;
+            align-items: center;
+        }
+
+        @media (max-width: 768px) {
+            min-width: 70px;
+        }
+    }
+
+    &:hover, &:active {
+        a {
+            span {
+                color: rgba(0, 0, 0, 0.9)
+            }
+        }
+    }
+`;
+
 
 export default Header;
