eb4cfcc4218a1ba0f1862c317ec053adca4cdc26
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-31 16:21:13 -0400

    add signout component

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,6 +53,9 @@ const Header = (props) => {
                                 <span>Me</span>
                                 <img src="/images/down-icon.svg" alt="" />
                             </a>
+                            <SignOut>
+                                <a>Sign Out</a>
+                            </SignOut>
                         </User>
                         <Work>
                             <a>
@@ -200,6 +203,17 @@ const NavList = styled.li`
     }
 `;
 
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 15px;
+    transition-duration: 167ms;
+    display: none;
+`;
 
 const User = styled(NavList)`
     a > svg {
@@ -215,10 +229,18 @@ const User = styled(NavList)`
         display: flex;
         align-items: center;
     }
+    &:hover {
+        ${SignOut} {
+            align-items: center;
+            justify-content: center;
+            display: flex;
+        }
+    }
 `;
 
 const Work = styled(User)`
-    border-left: 1px solid rgba(0, 0, 0, 0.08)
+    border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
+
 
 export default Header;
