07116d9d603627479731dc933ce24713f68f3712
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-31 16:33:57 -0400

    add subheader section
    refactor subheader to have Content wrapper around it
    
    init LEFT/MAIN/RIGHT Sections
    
    Add child components in LeftSide section
    
    Add LeftSection ===> CommunityCard Component
    
    finish off left-side section
    
    finish off left-side section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,80 @@
 import styled from 'styled-components'
+import Main from './Main';
+import Leftside from './Leftside';
+import Rightside from './Rightside';
 
 const Home = (props) => {
-    return <div>Home</div>
+    return (
+        <Container>
+            <Section>
+                <Content>
+                    <h5><a>Hirring in a hurry? - </a></h5>
+                    <p>Find talented pros in record time with Upwork and keep business moving.</p>
+                </Content>
+            </Section>
+            <Layout>
+                <Leftside />
+                <Main />
+                <Rightside />
+            </Layout>
+        </Container>    
+    )
 }
+
+const Container = styled.div`
+    padding-top: 52px;
+    max-width: 100%;
+`;
+
+const Section = styled.div`
+    min-height: 50px;
+    padding: 16px 0;
+    box-sizing: border-box;
+    text-align: center;
+    text-decoration: underline;
+    display: flex;
+    justify-content: center;
+    h5 {
+        color: #0a66c2;
+        font-size: 14px;
+        a {
+            font-weight: 700;
+        }
+    }
+
+    p {
+        font-size: 14px;
+        color: #434649;
+        font-weight: 600;
+    }
+
+    @media(max-width: 768px) {
+        flex-direction: column;
+        padding: 0 5px;
+    }
+`;
+
+const Content = styled.div`
+    max-width: 1128px;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside, main, rightside";
+    grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(0, 7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    /* grid-template-rows: auto; */
+    margin: 25px 0;
+
+    @media(max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding: 0 5px;
+    }
+`;
+
 
 export default Home;
---
+++ src/components/Rightside.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Rightside = (props) => {
+    return (
+        <Container>Right  Side</Container>
+    )
+}
+
+const Container = styled.div`
+    grid-area: rightside;
+`;
+
+export default Rightside;
